90bae91d59361172117661536a84f707aa9dfdb7
vlinku <vilius.linkunaitis@gmail.com>
2021-08-25 20:48:37 +0300

    Updated HomeWork2

--- works/HomeWork2.cs
+++ works/HomeWork2.cs
@@ -1,56 +1,43 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
 using OpenQA.Selenium.Firefox;
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace works
 {
   class HomeWork2
   {
+    private static IWebDriver Browser;
 
-    private static IWebDriver chrome;
-    private static IWebDriver firefox;
+    [TestCase("Chrome", "Chrome 92", TestName = "Chrome Browser")]
+    [TestCase("Firefox", "Firefox 91", TestName = "FireFox Browser")]
+   
 
-    [OneTimeSetUp]
-
-    public static void OneTimeSetUp()
+    public static void BrowserAndLocalOS(string compare, string browserName)
     {
-      chrome = new ChromeDriver();
-      chrome.Manage().Window.Maximize();
-      chrome.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-      chrome.Url = "https://developers.whatismybrowser.com/useragents/parse/?analyse-my-user-agent=yes#parse-useragent";
 
-      firefox = new FirefoxDriver();
-      firefox.Manage().Window.Maximize();
-      firefox.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-      firefox.Url = "https://developers.whatismybrowser.com/useragents/parse/?analyse-my-user-agent=yes#parse-useragent";
-
-    }
-
-    [OneTimeTearDown]
-
-    public static void OneTimeTearDown()
-    {
-      chrome.Quit();
-      firefox.Quit();
-    }
-
-    [TestCase("Chrome 92 on Windows 10", "Firefox 91 on Windows 10")]
-
-    public static void ChromeBrowserAndLocalOS(string ChromeBrowser, string FireFoxBrowser) 
-    {
-      IWebElement ChromeAndOS = chrome.FindElement(By.CssSelector("#primary-detection > div"));
-      IWebElement firefoxAndOS = firefox.FindElement(By.CssSelector(".simple-major"));
-     
-      Assert.AreEqual(ChromeBrowser, ChromeAndOS.Text, "Chrome browser and Operation system do not match!");
-      Assert.AreEqual(FireFoxBrowser, firefoxAndOS.Text, "FireFox browser and Operation system do not match!");
+      if ("Chrome".Equals(compare))
+      {
+        Browser = new ChromeDriver();
+      }
+      else if ("Firefox".Equals(compare))
+      {
+        Browser = new FirefoxDriver();
+      }
       
+
+
+      Browser.Manage().Window.Maximize();
+      Browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+      Browser.Navigate().GoToUrl("https://developers.whatismybrowser.com/useragents/parse/?analyse-my-user-agent=yes#parse-useragent");
+
+      IWebElement BrowserAndOS = Browser.FindElement(By.CssSelector("#primary-detection > div"));
+
+
+      Assert.IsTrue(BrowserAndOS.Text.Contains(browserName), $"Browser is not {compare}");
+      Browser.Quit();
     }
 
   }
