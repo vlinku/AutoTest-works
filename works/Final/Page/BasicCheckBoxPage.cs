ddd4cc657713e04c3078ef9c55d65995208c5b1f
vlinku <vilius.linkunaitis@gmail.com>
2021-09-02 22:18:41 +0300

    Final Project

---
+++ works/Final/Page/BasicCheckBoxPage.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace works.Final.Page
+{
+ public  class BasicCheckBoxPage : BasePage
+  {
+    private const string PageAddress = "https://www.seleniumeasy.com/test/basic-checkbox-demo.html";
+
+    private IWebElement _singleCheckBox => Driver.FindElement(By.Id("isAgeSelected"));
+
+    private IWebElement _singleCheckBoxMessage => Driver.FindElement(By.Id("txtAge"));
+
+    private const string SingleCheckBoxMessageText = "Success - Check box is checked";
+
+    private IReadOnlyCollection<IWebElement> _multipleCheckBoxes => Driver.FindElements(By.ClassName("cb1-element"));
+
+    private IWebElement _checkAllButton => Driver.FindElement(By.Id("check1"));
+
+
+    public BasicCheckBoxPage(IWebDriver webdriver) : base(webdriver)
+    { }
+
+    public BasicCheckBoxPage NavigateToPage()
+    {
+      if (Driver.Url != PageAddress)
+        Driver.Url = PageAddress;
+      return this;
+    }
+
+    public BasicCheckBoxPage CheckSingleCheckBox()
+    {
+      if (!_singleCheckBox.Selected)
+        _singleCheckBox.Click();
+      return this;
+    }
+
+    public BasicCheckBoxPage UnCheckSingleCheckBox()
+    {
+      if (_singleCheckBox.Selected)
+        _singleCheckBox.Click();
+      return this;
+    }
+
+    public BasicCheckBoxPage AssertSingleCheckBoxDemoSuccessMessage()
+    {
+
+      Assert.AreEqual(SingleCheckBoxMessageText, _singleCheckBoxMessage.Text, "tekstas nesutampa!");
+
+      return this;
+    }
+
+    public BasicCheckBoxPage AssertSingleCheckBoxDemoSuccessMessageWithWait()
+    {
+      Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+      Assert.AreEqual(SingleCheckBoxMessageText, _singleCheckBoxMessage.Text, "tekstas nesutampa!");
+
+      return this;
+    }
+
+
+    public BasicCheckBoxPage CheckAllMultipleCheckBoxes()
+    {
+      foreach (IWebElement singleCheckbox in _multipleCheckBoxes)
+      {
+        if (!singleCheckbox.Selected)
+          singleCheckbox.Click();
+      }
+      return this;
+    }
+
+    public BasicCheckBoxPage AssertButtonName(string expectedName)
+    {
+
+      Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+      Assert.AreEqual(expectedName, _checkAllButton.GetAttribute("value"), "Wrong button label");
+      return this;
+    }
+
+
+    public BasicCheckBoxPage ClickGroupButton()
+    {
+      _checkAllButton.Click();
+      return this;
+    }
+
+    public BasicCheckBoxPage AssertMultipleCheckBoxesUnchecked()
+    {
+      foreach (IWebElement singleCheckbox in _multipleCheckBoxes)
+      {
+        Assert.False(singleCheckbox.Selected, "One of checkboxes is still checked!");
+      }
+
+      return this;
+    }
+
+  }
+}
---
+++ works/Final/Test/BaseTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using works.Final.Page;
+using works.Final.Test;
+using works.Final.Tools;
+
+namespace works.Final.Test
+{
+  public class BaseTest
+  {
+    public static IWebDriver driver;
+    public static BasicCheckBoxPage _page;
+
+
+    [OneTimeSetUp]
+    public static void SetUp()
+    {
+      driver = CustomDriver.GetChromeDriver();
+
+      _page = new BasicCheckBoxPage(driver);
+    }
+
+    [TearDown]
+    public static void TakeScreeshot()
+    {
+      if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+        MyScreenshot.MakeScreeshot(driver);
+    }
+
+    [OneTimeTearDown]
+    public static void TearDown()
+    {
+      driver.Quit();
+    }
+  }
+}
